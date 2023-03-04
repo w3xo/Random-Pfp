97925d8b12cafac30534988695457dc8a5702031
w3xo <96422317+w3xo@users.noreply.github.com>
2023-03-04 15:09:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 class config {
     constructor() {
-        this.token = "TOKEN"
+        this.token = "MTA4MTU5Mzc2NjgwMjEwODQ2Ng.GJY_C4.-443UPnXlvhmJKdtVPqsJThPH9QBGdL8uVML_c"
         this.time = 10000
         this.color = "480ca8"
         this.channels = []
