5d67953a1dc2c77c10900b3337609ab720ea74d9
w3xo <96422317+w3xo@users.noreply.github.com>
2023-03-04 15:19:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ client.on("ready", () => {
                 embeds: [{
                     author: { iconURL: user.displayAvatarURL({ dynamic: true, format: "png", size: 2048 }),  name: user.tag },
                     description: `[PNG](${user.displayAvatarURL({ size: 2048, format: "png" })}) - [JPEG](${user.displayAvatarURL({ size: 2048, format: "jpeg" })}) - [JPG](${user.displayAvatarURL({ size: 2048, format: "jpg" })}) - [WEBP](${user.displayAvatarURL({ size: 2048, format: "webp" })})`,
-                    footer: { text: "Made by Kaito. ❤️" },
+                    footer: { text: "Created by alive." },
                     image: { url: user.displayAvatarURL({ dynamic: true, format: "png", size: 2048 }) },
                     color: config.color
                 }]
